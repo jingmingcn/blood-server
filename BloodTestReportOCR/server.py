0f4fd80e9d3bd3ffe92094188ebf1a8967387456
Ming Jing <jingming.cn@gmail.com>
2019-11-21 15:32:23 +0800

    Update server.py

--- BloodTestReportOCR/server.py
+++ BloodTestReportOCR/server.py
@@ -121,7 +121,7 @@ def image_upload():
         }
         return jsonify(data)
     else:
-        return jsonify(report_data)
+        return json.dumps(report_data, ensure_ascii=False).encode('utf8')
 '''
     根据图像oid，在mongodb中查询，并返回Binary对象
 '''
