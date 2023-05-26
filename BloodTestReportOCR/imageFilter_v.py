bbd7ca7641ee7e2488fa704168f3575605318946
Ming Jing <jingming.cn@gmail.com>
2023-05-26 11:12:22 +0800

    Update imageFilter_v.py

--- BloodTestReportOCR/imageFilter_v.py
+++ BloodTestReportOCR/imageFilter_v.py
@@ -425,7 +425,7 @@ class ImageFilter:
             print(digtitstr)
             data['bloodtest'][item_num]['value'] = digtitstr
         
-        data['profile']['gender'] = image_to_string(read('temp_pics/gender.jpg'),False).replace(" ", '').replace("\n", '').strip()
+        data['profile']['gender'] = image_to_string(read('temp_pics/gender.jpg'),True).replace(" ", '').replace("\n", '').strip()
         data['profile']['age'] = image_to_string(read('temp_pics/age.jpg'),True).replace(" ", '').replace("\n", '')
 
         return data
