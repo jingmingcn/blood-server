76ace1b0564849be45440b90fc48b44d3b92605d
Ming Jing <jingming.cn@gmail.com>
2023-05-26 11:18:34 +0800

    Update server.py

--- BloodTestReportOCR/server.py
+++ BloodTestReportOCR/server.py
@@ -39,7 +39,7 @@ lgbm_2class_np = pk.load(open("model/gbm_2class_np.pkl", "rb"))
 # '急性髓样白血病' '淋巴瘤' '血小板减少性紫癜' '过敏性紫癜[亨诺克(－舍恩莱因)紫癜]' '骨髓增生异常综合征']
 lgbm_9class_positive = pk.load(open("model/gbm_9class_positive.pkl", "rb"))
 
-diseases = ['再生障碍性贫血', 
+diseases = ['','再生障碍性贫血', 
             '噬血细胞综合症', 
             '多发性骨髓瘤', 
             '急性淋巴细胞白血病',
